3d793f0198de6af1bd2b334999b812a35dae3e3a
Void & Null <kosmix1999@gmail.com>
2024-05-01 10:27:00 -0500

    Source Code

---
+++ Source/HYWMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Verse;
+namespace NoWealthStorageZone;
+
+public class HywMod : Mod
+{
+    
+    public HywMod(ModContentPack content) : base(content)
+    {
+        var harmony = new Harmony("vnull.HideYourWealth");
+        harmony.PatchAll();
+    }
+}
