bf738a94e0337ce53a946d81580beed9524940ad
Void & Null <kosmix1999@gmail.com>
2024-05-01 11:56:00 -0500

    Added Docs to HwyMod

--- Source/HYWMod.cs
+++ Source/HYWMod.cs
@@ -2,9 +2,11 @@
 using Verse;
 namespace NoWealthStorageZone;
 
+/// <summary>
+/// The main class for the Hide Your Wealth mod.
+/// </summary>
 public class HywMod : Mod
 {
-    
     public HywMod(ModContentPack content) : base(content)
     {
         var harmony = new Harmony("vnull.HideYourWealth");
